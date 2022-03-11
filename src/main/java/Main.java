80345d1c647c9057a7b7ecc4b14ebc105180ab6e
Luis Campano Goldar <lcampanogoldar@danielcastelao.org>
2022-03-11 09:35:52 +0100

    Commit Main.

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int op = 0;
+
+        switch (op) {
+
+            case 0:
+                Repositorio rep = new Repositorio();
+                rep.CrearRepositorio();
+
+
+
+
+        }
+    }
+}
